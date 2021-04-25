b798e73a87e4091e38680b9d3c10d0dd607f14fa
c110106 <3036614038@qq.com>
2021-04-25 15:05:19 +0800

    上传

--- src/practice/CommercialBank.java
+++ src/practice/CommercialBank.java
@@ -7,7 +7,7 @@ public class CommercialBank extends Bank{
         double r = year-(int)year;
         int day= (int)(r*1000);
         double yearInterest =super.computerInterest();
-        double dayInterest = day*0.00015*savedMoney;
+        double dayInterest = day*0.00035*savedMoney;
         interest= yearInterest+dayInterest;
         System.out.printf("%d元存在商业银行%d年零%d天的利息：%f元\n",savedMoney,super.year,day,interest);
         return interest;
